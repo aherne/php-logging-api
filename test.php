a2b873802222b1ac03e3dfbbf2e66419ae066f77
Lucian Gabriel Popescu <lucian.gabriel.popescu@gmail.com>
2026-04-07 06:42:49 +0300

    php8.5 compatibility

--- test.php
+++ test.php
@@ -1,7 +1,11 @@
 <?php
 require __DIR__ . '/vendor/autoload.php';
 try {
-    new Lucinda\UnitTest\ConsoleController("unit-tests.xml", "local");
-} catch (Exception $e) {
-    echo $e->getMessage();
+    if (!file_exists("/var/log/syslog")) {
+        throw new Exception("Syslog not installed in: /var/log/syslog");
+    }
+    new Lucinda\UnitTest\ConsoleController("unit-tests.xml", "local", ($argv[1] ?? null));
+} catch (\Throwable $e) {
+    echo "ERROR: ".$e->getMessage().PHP_EOL;
+    echo "TRACE: ".$e->getTraceAsString();
 }
